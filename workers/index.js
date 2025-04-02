48e176f190dd7ae1c41fa4477ca490ce96d797c6
Aaron Hasty <aaron.hasty@8tap.com>
2025-04-02 13:22:38 -0500

    new Data Page

--- workers/index.js
+++ workers/index.js
@@ -1,4 +1,5 @@
 import mainHTML from "./main.html";
+import resultHTML from "./result.html";
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
@@ -59,22 +60,14 @@ async function renderSession(sessionId, env) {
         return `<html><body><h1>No data captured yet for ${sessionId}</h1></body></html>`;
     }
     const session = results[0];
-
-    return `
-    <html>
-    <head><title>Session ${sessionId}</title></head>
-    <body>
-        <h1>Session ${sessionId}</h1>
-        <p><strong>Method:</strong> ${session.method}</p>
-        <p><strong>Headers:</strong></p>
-        <pre>${JSON.stringify(JSON.parse(session.headers), null, 2)}</pre>
-        <p><strong>Body:</strong></p>
-        <pre>${session.body}</pre>
-        <p><strong>Query Parameters:</strong></p>
-        <pre>${JSON.stringify(JSON.parse(session.query_params), null, 2)}</pre>
-    </body>
-    </html>
-    `;
+    headersJSON = JSON.stringify(JSON.parse(session.headers), null, 2)
+    paramsJSON = JSON.stringify(JSON.parse(session.query_params), null, 2)
+    let HTML = resultHTML.replace(/{{sessionId}}/g, sessionId)
+                         .replace(/{{session.method}}/g, session.method)
+                         .replace(/{{headersJSON}}/g, headersJSON)
+                         .replace(/{{session.body}}/g, session.body)
+                         .replace(/{{paramsJSON}}/g, paramsJSON);
+    return HTML;
 }
 
 async function cleanupOldSessions(env) {
